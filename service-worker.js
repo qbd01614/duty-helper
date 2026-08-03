f7780369a6f36059ce9c7d23327da93108f48784
qbd01614 <qbd01614@gmail.com>
2026-08-03 22:50:18 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE="duty-helper-v21-20260803";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"])))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
