c68ccd5f4e6bc2a56b2c9b9244148762f5c3add8
qbd01614 <qbd01614@gmail.com>
2026-08-04 15:34:22 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,9 @@
-const VERSION="V5.4";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim())));self.addEventListener("fetch",()=>{});
+const VERSION="V5.7";
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+    .then(()=>self.registration.unregister())
+    .then(()=>self.clients.claim())
+));
+self.addEventListener("fetch",()=>{});
